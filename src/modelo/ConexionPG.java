9bd647ec9a4ccb47d71e46a71fa4c55caa2e97bc
edisson23 <63137777+edisson23@users.noreply.github.com>
2021-03-25 07:46:46 -0500

    Inicializando repositario en github.

---
+++ src/modelo/ConexionPG.java
@@ -0,0 +1,45 @@
+
+package modelo;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+public class ConexionPG {
+     
+    private String url = "jdbc:postgresql://localhost:5432/ARTEX";
+    private String usuario = "postgres";
+    private String password = "123ASDFG" ;
+    private Connection conexion;
+    
+    public ConexionPG(){
+        
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(ConexionPG.class.getName()).log(Level.SEVERE, null, ex);
+        }  
+        
+        try {
+            conexion = DriverManager.getConnection(url, usuario, password);
+            System.out.println("CONEXION EXITOSA");
+        } catch (SQLException ex) {
+            Logger.getLogger(ConexionPG.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println("NO SE HA PODIDO ESTABLECER LA CONEXION");
+        }
+        
+    }
+
+    public Connection getConexion() {
+        return conexion;
+    }
+
+    public void setConexion(Connection conexion) {
+        this.conexion = conexion;
+    }
+
+    
+}
---
+++ src/mvc/MVC.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mvc;
+
+import vista.vistaPersona;
+
+/**
+ *
+ * @author Steve
+ */
+public class MVC {
+
+    public static void main(String[] args) {
+        
+         /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new vistaPersona().setVisible(true);
+            }
+        });
+        }
+    }
